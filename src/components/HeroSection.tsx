13d77949cf03b3e3aa3a6dfd4b5f06edcb79f3b9
GOODBOYKITTU272 <chandaramakrishna2013@gmail.com>
2026-04-10 15:09:47 +0530

    Updated resume download link

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -102,7 +102,7 @@ const HeroSection = () => {
               </Link>
 
               {/* Download Resume Button */}
-              <a href="/resume.pdf" download>
+              <a href="/rupesh_resume.pdf" download="Rupesh_Garrepalli_Resume.pdf">
                 <button className="px-6 py-3 bg-secondary text-white font-semibold rounded-full hover:bg-secondary/90 transition-all">
                   Download Resume
                 </button>
